85ed1318b8962ef2efbf813dc8c2e317ec7288de
unknown <nguyenbathucpr@gmail.com>
2026-02-26 23:33:28 +0700

    add logic

--- src/components/wedding/GallerySection.tsx
+++ src/components/wedding/GallerySection.tsx
@@ -9,12 +9,8 @@ import g4 from "../../image/g4.jpg";
 import g5 from "../../image/g5.jpg";
 import g6 from "../../image/g6.jpg";
 import g7 from "../../image/g7.jpg";
-import g8 from "../../image/g8.jpg";
-import g9 from "../../image/g9.jpg";
-import g10 from "../../image/g10.jpg";
-import g11 from "../../image/g11.jpg";
 
-const galleryImages = [g0, g1, g2, g3, g4, g5, g6, g7, g8, g9, g10, g11];
+const galleryImages = [g0, g1, g2, g3, g4, g5, g6, g7];
 const GallerySection = () => {
   const [selectedImage, setSelectedImage] = useState<number | null>(null);
 
