58b88945b3b548fff512598347f6ccbae2907168
thucnb <thucnb@newwave.com.vn>
2026-02-27 08:27:15 +0700

    hahaah

--- src/components/wedding/GallerySection.tsx
+++ src/components/wedding/GallerySection.tsx
@@ -1,14 +1,14 @@
 import { useState } from 'react';
 import { X, ChevronLeft, ChevronRight } from 'lucide-react';
 
-import g0 from "../../image/g0.jpg";
-import g1 from "../../image/g1.jpg";
-import g2 from "../../image/g2.jpg";
-import g3 from "../../image/g3.jpg";
-import g4 from "../../image/g4.jpg";
-import g5 from "../../image/g5.jpg";
-import g6 from "../../image/g6.jpg";
-import g7 from "../../image/g7.jpg";
+import g0 from "../../image/g0.webp";
+import g1 from "../../image/g1.webp";
+import g2 from "../../image/g2.webp";
+import g3 from "../../image/g3.webp";
+import g4 from "../../image/g4.webp";
+import g5 from "../../image/g5.webp";
+import g6 from "../../image/g6.webp";
+import g7 from "../../image/g7.webp";
 
 const galleryImages = [g0, g1, g2, g3, g4, g5, g6, g7];
 const GallerySection = () => {
